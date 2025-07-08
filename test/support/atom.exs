80f2c6013eb132987ee2f51bf712702ef9af4d90
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2025-07-08 05:12:19 +0000

    add atom test script, run from ci

---
+++ test/support/atom.exs
@@ -0,0 +1,9 @@
+WebPushEx.Subscription.from_json("""
+{
+  "endpoint": "https://push.example.com/123",
+  "keys": {
+    "p256dh": "BCVxsr7N_eNgVRqvHtD0zTZsEc6-VV-JvLexhqUzORcxaOzi6-AYWXvTBHm4bjyPjs7Vd8pZGH6SRpkNtoIAiw4",
+    "auth": "BTBZMqHH6r4Tts7J_aSIgg"
+  }
+}
+""")
